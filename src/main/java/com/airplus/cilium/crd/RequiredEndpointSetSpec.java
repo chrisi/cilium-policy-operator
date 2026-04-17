3803fce93c1609e67a12348a1f0120f00de89e88
Christian Gebauer <mail@christiangebauer.de>
2026-04-17 09:55:58 +0200

    add target match labels

--- src/main/java/com/airplus/cilium/crd/RequiredEndpointSetSpec.java
+++ src/main/java/com/airplus/cilium/crd/RequiredEndpointSetSpec.java
@@ -1,8 +1,15 @@
 package com.airplus.cilium.crd;
 
-import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
 public class RequiredEndpointSetSpec {
+  private Map<String, String> targetMatchLabels;
   private List<String> predefinedEndpoints;
   private List<Endpoint> customEndpoints;
 }
